d02fa0fbade051a0635af6830252e593aa0cb1c3
Pagidi Sai Praneeth <pagidichembulasaipraneeth777@gmail.com>
2026-07-31 18:32:22 +0000

    Initial implementation of AWS resource tracker

--- aws_resource_tracker.sh
+++ aws_resource_tracker.sh
@@ -1,2 +1,27 @@
 #!/bin/bash
 
+#######################
+#
+# Author:- Sai
+# Date:- 31st -Jul
+#
+#
+# version:- v1
+#
+#
+# This script will report the AWS resource usage#
+#
+#######################
+
+#AWS s3
+#AWS EC2
+#AWS lambda
+#AWS IAM users
+
+aws s3 ls
+
+aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+aws lambda list-functions
+
+aws iam list-users
